0b58a8bd72fdc1874c9d4debf2b480abd0522629
abalam1980 <emailbrian.reijnders@gmail.com>
2021-04-22 09:23:49 +0200

    [2/4]Port faceunlock

--- config/common.mk
+++ config/common.mk
@@ -89,3 +89,15 @@ include vendor/aosip/config/packages.mk
 
 # Versioning
 include vendor/aosip/config/version.mk
+
+# Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := true
+ifneq ($(TARGET_DISABLE_ALTERNATIVE_FACE_UNLOCK), true)
+PRODUCT_PACKAGES += \
+    FaceUnlockService
+TARGET_FACE_UNLOCK_SUPPORTED := true
+endif
+PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
+    ro.face.moto_unlock_service=$(TARGET_FACE_UNLOCK_SUPPORTED)
+
+
